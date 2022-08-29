03a6028e6ec62bacd215a1db4b89e01fbb64765b
Chiara Cintioni <83300887+chiara-cintioni@users.noreply.github.com>
2022-08-29 09:36:00 +0200

    Codice per rimozione di duplicati specifici
    La rimozione dei duplicati avviene in base al campo organism name, per cui gli organismi presenti nel documento risultano univoci.

--- Python lettuta file/prova3Funziona.py
+++ Python lettuta file/prova3Funziona.py
@@ -1,6 +1,7 @@
 
 
 import pandas as pd
+
 inputFile = input("Insert file name with file extension (txt) to modify: ")
 outputFile = input("Insert file name with file extension (csv) where saves file raws without duplicates: ")
 data = pd.read_table(inputFile, low_memory=False)
