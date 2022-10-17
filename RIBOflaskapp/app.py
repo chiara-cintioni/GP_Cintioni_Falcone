463c65b38e5aff3853ef00cbad887e784498a385
DeniseFalcone <denise.falcone@studenti.unicam.it>
2022-10-17 18:04:57 +0200

    Finito ricerca
    Finita la ricerca nel db (manca la tassonomia). Iniziata l'implementazione della selezione dei risultati per il download.
    
    Co-Authored-By: Chiara Cintioni <83300887+chiara-cintioni@users.noreply.github.com>

--- RIBOflaskapp/app.py
+++ RIBOflaskapp/app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, url_for, redirect
 from pymongo import MongoClient
 import search_script
 
+
 # usiamo l'oggetto Flask per creare l'istanza della nostra app Flask con il nome app.
 # __name__ è il nome del modulo python corrente. Usato per dire all'app dove si trova. Serve perché Flask definisce dei path dietro le quinte.
 app = Flask(__name__)
@@ -21,23 +22,37 @@ def search():
     return render_template('search.html')
 
 
-@app.route('/search/result', methods=['GET', 'POST'])
-def search_db():
+@app.route('/download/', methods=['GET', 'POST'])
+def download():
     if request.method == 'POST':
-        if request.form.get()
-            redirect("/search/", code=302)
         org_name = request.form['Organism name']
-        length = request.form['Length']
-        if length == '':
-            length = -1
+        any_length = request.form['Length']
+        from_length = request.form['Length_from']
+        to_length = request.form['Length_to']
+        if any_length != '' and (from_length != '' or to_length != ''):
+            return redirect('/search/', 302, None)
+        if any_length == '':
+            any_length = -1
+        if from_length == '':
+            from_length = -1
+        if to_length == '':
+            to_length = -1
         acc_num = request.form['Accession number']
         benchmark = request.form['Benchmark ID']
         num_dec = request.form['Number of decoupled nucleotides']
         if num_dec == '':
             num_dec = -1
-        num_weak_bonds = request.form['Number of weak bonds']
-        if num_weak_bonds == '':
-            num_weak_bonds = -1
+        any_weak = request.form['Number_of_weak_bonds']
+        weak_from = request.form['Number_of_weak_bonds_from']
+        weak_to = request.form['Number_of_weak_bonds_to']
+        if any_weak != '' and (weak_from != '' or weak_to != ''):
+            return redirect('/search/', 302, None)
+        if any_weak == '':
+            any_weak = -1
+        if weak_from == '':
+            weak_from = -1
+        if weak_to == '':
+            weak_to = -1
         is_pseudoknotted = request.form['Is Pseudoknotted']
         pseudoknot_ord = request.form['Pseudoknot order']
         if pseudoknot_ord == '':
@@ -46,14 +61,18 @@ def search_db():
         genus = request.form['Genus']
         if genus == '':
             genus = -1
+        taxonomy = request.form['Taxonomy']
+        domain = request.form['Domain']
         core = request.form['Core']
         core_plus = request.form['Core plus']
         shape = request.form['Shape']
         result_org_name = search_script.search_org_name(org_name, collection)
         result_acc_num = search_script.search_acc_num(acc_num, result_org_name)
-        result_length = search_script.search_length(int(length), result_acc_num)
+        result_length = search_script.search_weak_bonds(int(from_length), int(to_length), int(any_length),
+                                                        result_acc_num)
         result_decoupled = search_script.search_num_decoup(int(num_dec), result_length)
-        result_weak_bonds = search_script.search_weak_bonds(int(num_weak_bonds), result_decoupled)
+        result_weak_bonds = search_script.search_weak_bonds(int(weak_from), int(weak_to), int(any_weak),
+                                                            result_decoupled)
         result_is_pseudo = search_script.search_is_pseudo(is_pseudoknotted, result_weak_bonds)
         result_pseudo_ord = search_script.search_pseudo_order(int(pseudoknot_ord), result_is_pseudo)
         result_rna_type = search_script.search_rna_type(rna_type, result_pseudo_ord)
@@ -62,10 +81,12 @@ def search_db():
         result_core_plus = search_script.search_core_plus(core_plus, result_core)
         result_shape = search_script.search_shape(shape, result_core_plus)
         result_benchmark = search_script.search_benchmark(benchmark, result_shape)
-        result = result_benchmark.find()
+        result_taxonomy = search_script.search_taxonomy(taxonomy, result_benchmark)
+        result_domain = search_script.search_domain(domain, result_taxonomy)
+        result = result_domain.find()
         return render_template("form.html", result_list=result)
 
-    return render_template('form.html')
+    return render_template('download.html')
 
 
 @app.route('/')
@@ -91,11 +112,6 @@ def s_tutorial():
 @app.route('/download_tutorial')
 def d_tutorial():
     return render_template('d_tutorial.html')
-
-
-@app.route('/download/')
-def download():
-    return render_template('download.html')
 
 
 @app.route('/info/')
--- RIBOflaskapp/static/js/script.js
+++ RIBOflaskapp/static/js/script.js
@@ -1,23 +1,49 @@
+import $ from "jquery";
+
 function check_input_length(){
-     var any = document.forms.RegForm.Length.value;
-     var from = document.forms.RegForm.Length_from.value;
-     var to = document.forms.RegForm.Length_to.value;
-     if (any !== '' && from !== '' || to !== ''){
-        window.alert("You can only enter a specific length or a range.");
-        any.focus();
-        return "No";
-     }
-     return "Ok"
+    var any = document.forms.RegForm.Length.value
+    var from = document.forms.RegForm.Length_from.value
+    var to = document.forms.RegForm.Length_to.value
+    if (any !== '' && (from !== '' || to !== '')){
+        window.alert("You can only enter a specific length or a range.")
+        return false
+    }
+    return true
 }
+
 
 function check_input_weak_bonds(){
      var any = document.forms.RegForm.Number_of_weak_bonds.value;
      var from = document.forms.RegForm.Number_of_weak_bonds_from.value;
      var to = document.forms.RegForm.Number_of_weak_bonds_to.value;
-     if (any !== '' && from !== '' || to !== ''){
+     if (any !== '' && (from !== '' || to !== '')){
         window.alert("You can only enter a specific number of base pairs or a range.");
-        any.focus();
         return false;
      }
      return true
 }
+
+$(function() {
+	//If check_all checked then check all table rows
+	$("#check_all").on("click", function () {
+		if ($("input:checkbox").prop("checked")) {
+			$("input:checkbox[name='row-check']").prop("checked", true);
+		} else {
+			$("input:checkbox[name='row-check']").prop("checked", false);
+		}
+	});
+
+	// Check each table row checkbox
+	$("input:checkbox[name='row-check']").on("change", function () {
+		let total_check_boxes = $("input:checkbox[name='row-check']").length;
+		let total_checked_boxes = $("input:checkbox[name='row-check']:checked").length;
+
+		// If all checked manually then check check_all checkbox
+		if (total_check_boxes === total_checked_boxes) {
+			$("#check_all").prop("checked", true);
+		}
+		else {
+			$("#check_all").prop("checked", false);
+		}
+	});
+});
